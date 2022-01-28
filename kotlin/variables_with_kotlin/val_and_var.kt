133783014840ffded594d3a5cee554aab19924da
Toushik Banik <89782544+ToushikBanik123@users.noreply.github.com>
2022-01-28 18:32:42 +0530

    Create val_and_var.kt

---
+++ kotlin/variables_with_kotlin/val_and_var.kt
@@ -0,0 +1,17 @@
+fun main() {
+	// mutated variable in kotlin :
+	// this are the variable's value can be change just like a variable in c or c++
+	// we use ' var ' keyword for a mutable variable 
+	//example : 
+	var a =3
+    a = 6
+    println(a)
+    
+    //immutable variable in kotlin:
+    //this are the variable's value can not be changed just like a constant in c or c++
+    //we use ' val ' keyword for a immutable variable
+    //example:
+    val name = "Toushik"
+    // name = "Banik" - you cant do things like this with val
+    println(name)
+}
